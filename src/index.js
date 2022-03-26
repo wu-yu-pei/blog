04cbce8883bd4c7d221e6ff76c0b1f147befa6bf
wuyupei <495174699@qq.com>
2022-03-26 16:14:58 +0800

    index.js---run函数

--- src/index.js
+++ src/index.js
@@ -2,9 +2,12 @@ const { mdToml } = require('./core/mdToml');
 
 const { createListHtml } = require('./core/createList');
 
+function run() {
+  // markdown 转为 html
+  mdToml();
 
-// markdown 转为 html
-mdToml();
+  // 生成list列表页
+  createListHtml();
+}
 
-// 生成list列表页
-createListHtml();
+run();
