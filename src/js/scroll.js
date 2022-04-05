67c43ebd4ec3e724a9a652d966c7b92de679d947
wuyupei <495174699@qq.com>
2022-04-05 11:30:47 +0800

    scroll bg

--- src/js/scroll.js
+++ src/js/scroll.js
@@ -3,16 +3,16 @@
   MyScrollBar.innerHTML = `
          <style>
               ::-webkit-scrollbar {
-                  background-color: #304156;
+                  background-color: #fff;
                   width: 6px;
                   overflow: visible;
               }
               ::-webkit-scrollbar-thumb {
-                  background-color: #2474b5;
+                  background-color: #304156;
                   border-radius: 0;
               }
               ::-webkit-scrollbar-button {
-                  background-color: #2474b5;
+                  background-color: #304156;
                   width:0px;
                   height:0px;
               }
@@ -26,11 +26,11 @@
                 overflow: visible;
               }
               .content::-webkit-scrollbar-thumb {
-                  background-color: #2474b5;
+                  background-color: #304156;
                   border-radius: 0;
               }
               .content::-webkit-scrollbar-button {
-                  background-color: #2474b5;
+                  background-color: #304156;
                   width:0px;
                   height:0px;
               }
@@ -44,11 +44,11 @@
                 overflow: visible;
               }
               pre::-webkit-scrollbar-thumb {
-                  background-color: #2474b5;
+                  background-color: #304156;
                   border-radius: 0;
               }
               pre::-webkit-scrollbar-button {
-                  background-color: #2474b5;
+                  background-color: #304156;
                   width:0px;
                   height:0px;
               }
