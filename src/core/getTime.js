6f14959e69f1b0a29050ea5059c39ae8631ded53
wuyupei <495174699@qq.com>
2022-04-10 15:42:04 +0800

    list layout

--- src/core/getTime.js
+++ src/core/getTime.js
@@ -3,15 +3,13 @@
  */
 function getTime(t) {
   let time = new Date(t);
-  return (
-    [time.getFullYear(), time.getMonth() + 1, time.getDate()].map(item => formart(item)).join('-') +
-    ' ' +
-    [time.getHours(), time.getMinutes(), time.getSeconds()].map(item => formart(item)).join(':')
-  );
+  return [time.getFullYear(), time.getMonth() + 1, time.getDate()]
+    .map((item) => formart(item))
+    .join('-');
 }
 
 function formart(time) {
-  return time < 10 ? '0'+time : time
+  return time < 10 ? '0' + time : time;
 }
 
 module.exports = { getTime };
