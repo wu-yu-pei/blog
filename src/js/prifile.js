9a77df1faf9c0d964eedcc1d961182bb04515665
wuyupei <495174699@qq.com>
2022-04-10 21:23:19 +0800

    sessionStorage

--- src/js/prifile.js
+++ src/js/prifile.js
@@ -1,7 +1,7 @@
-let hasToken = JSON.parse(localStorage.getItem('Token')) || prompt('请输入密码');
+let hasToken = JSON.parse(sessionStorage.getItem('Token')) || prompt('请输入密码');
 if (hasToken != '1978') {
   alert('密码错误');
   history.go(-1);
 } else {
-  localStorage.setItem('Token', '1978');
+  sessionStorage.setItem('Token', '1978');
 }
