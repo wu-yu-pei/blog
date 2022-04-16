4b66c7a6be2b48a2ed23d8ef1d719cc35131cf36
wuyupei <495174699@qq.com>
2022-04-16 20:58:44 +0800

    rm:server.js data

--- server.js
+++ server.js
@@ -1,24 +1,14 @@
-// 开启服务器访问: express
-// test curl:
-//             method  command  params  methoud  params    value                         url
-//             head:     curl    -I                                               http://127.0.0.1:8000
-//             put:      curl    -T                                               http://127.0.0.1:8000/hi
-//             get:      curl                                                     http://127.0.0.1:8000/hi
-//             post:     curl    -X      POST   --date(/-d) "name=wuyupei&age=20" http://127.0.0.1:8000/hi
-//             delete:   curl    -X      DELETE                                   http://127.0.0.1:8000/hi
 const express = require('express');
 
 const app = express();
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
 
 app.get('/hi/:id', (req, res) => {
   res.send({ id: req.params });
 });
 
-app.post('/hi', (req, res) => {
-  res.send({ data: JSON.stringify(req.body) });
+app.get('/page/:id', (req, res) => {
+  
+  res.send({ id: req.params.id });
 });
 
 // 开启静态文件夹: ./
