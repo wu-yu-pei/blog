b6fb9795bd5032c727e1c9ea2c0f6194660719c3
wuyupei <495174699@qq.com>
2022-04-16 22:45:06 +0800

    分页

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
 const express = require('express');
-
+const { createListHtmlTwo } = require('./src/core/createList');
 const app = express();
 
 app.get('/hi/:id', (req, res) => {
   res.send({ id: req.params });
 });
 
-app.get('/page/:id', (req, res) => {
-  
-  res.send({ id: req.params.id });
+app.get('/page/:page', (req, res) => {
+  let { page } = req.params;
+
+  page = page ? page : 0;
+
+  createListHtmlTwo(page, count = 14);
+  res.send({ page });
 });
 
 // 开启静态文件夹: ./
--- src/core/createList.js
+++ src/core/createList.js
@@ -23,12 +23,34 @@ function createListHtml() {
   // 排序
   info.sort((a, b) => b.time - a.time);
 
-  ejs.renderFile('./ejs/list.ejs', { data: info }, {}, function (err, str) {
-    if (err) {
-      console.log(err);
-    }
-    fs.writeFileSync('../list.html', str, { encoding: 'utf-8' });
-  });
+  let result = [];
+  let start = 0;
+  console.log('共:' +  info.length + '篇');
+  while (start <= info.length) {
+    result.push(info.slice(start, start + 10));
+    start += 10;
+  }
+
+  for (let i = 0; i < result.length; i++) {
+    ejs.renderFile(
+      './ejs/list.ejs',
+      { data: result[i], page: result.length - i, totalPage: result.length },
+      {},
+      function (err, str) {
+        if (err) {
+          console.log(err);
+        }
+        if (i == 0) {
+          fs.writeFileSync(`../pageList/index.html`, str, {
+            encoding: 'utf-8',
+          });
+        }
+        fs.writeFileSync(`../pageList/list${result.length - i}.html`, str, {
+          encoding: 'utf-8',
+        });
+      }
+    );
+  }
 }
 
 module.exports = { createListHtml };
