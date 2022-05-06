3c2a9a5ce0a06bdb3da60b8f0a049bd12d19c20f
wuyupei <495174699@qq.com>
2022-05-06 15:41:48 +0800

    fix: progress

--- src/js/notify.js
+++ src/js/notify.js
@@ -36,13 +36,16 @@ function createProgress() {
   const bodyHieght = document.body.clientHeight;
 
   progress.setAttribute('max', '100');
-  progress.setAttribute('value', '40');
+  progress.setAttribute('value', '0');
   document.body.appendChild(progress);
 
   document.addEventListener('scroll', (e) => {
     const scrollTop = document.documentElement.scrollTop;
 
-    progress.setAttribute('value', 100 * (scrollTop / (bodyHieght - window.innerHeight - 62 - 100)));
+    progress.setAttribute(
+      'value',
+      100 * (scrollTop / (bodyHieght - window.innerHeight - 62 - 100))
+    );
   });
 }
 createProgress();
